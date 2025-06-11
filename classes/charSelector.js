c709dc18b192a8913ce794a6101375af2dc00a94
TaureHorn <6367950+TaureHorn@users.noreply.github.com>
2025-06-11 16:30:19 +0100

    css and styling done for v13

--- classes/charSelector.js
+++ classes/charSelector.js
@@ -1,105 +1,6 @@
 import ACP from "../active-character-portrait.js";
 const { ApplicationV2, HandlebarsApplicationMixin } = foundry.applications.api
 
-// TODO: convert to ApplicationV2
-class CharacterSelector extends FormApplication {
-
-    static get defaultOptions() {
-        const defaults = super.defaultOptions;
-        const overriders = {
-            closeOnSubmit: false,
-            height: game.user.getFlag(ACP.ID, ACP.FLAGS.SELECTOR).height,
-            id: `${ACP.ID}_character-selector`,
-            left: game.user.getFlag(ACP.ID, ACP.FLAGS.SELECTOR).left,
-            minimizable: false,
-            popOut: true,
-            query: "",
-            resizable: true,
-            template: ACP.TEMPLATE.CHARSELECT,
-            title: 'Character Selector',
-            top: game.user.getFlag(ACP.ID, ACP.FLAGS.SELECTOR).top,
-            width: game.user.getFlag(ACP.ID, ACP.FLAGS.SELECTOR).width,
-        }
-        return foundry.utils.mergeObject(defaults, overriders)
-    }
-
-    getData() {
-
-        let characters = []
-        if (!this.options.query) {
-            characters = game.actors._source
-        } else {
-            characters = game.actors.search({ "query": this.options.query })
-        }
-        if (!game.user.isGM) {
-            characters = characters.filter((obj) => obj.ownership.default === 3 || obj.ownership.hasOwnProperty(game.userId))
-        }
-
-        return {
-            chars: characters,
-            user: game.user
-        }
-    }
-
-    _getHeaderButtons() {
-        return [
-            {
-                class: "pin",
-                icon: "fas fa-thumbtack",
-                label: game.settings.get(ACP.ID, 'headerLabels') ? 'Pin' : '',
-                onclick: () => {
-                    game.user.setFlag(ACP.ID, ACP.FLAGS.SELECTOR, this.position)
-                    ui.notifications.notify("active character portrait | Pinned character selector to current size and position")
-                },
-                tooltip: 'Remember this window size and location'
-            },
-            {
-                class: "close",
-                icon: "fas fa-times",
-                label: game.settings.get(ACP.ID, 'headerLabels') ? 'Close' : '',
-                onclick: () => this.close(),
-                tooltip: 'Close window'
-            }
-        ]
-    }
-
-    activateListeners(html) {
-        super.activateListeners(html)
-        html.on('click', "[data-action]", this._handleButtonClick)
-    }
-
-    _handleButtonClick = (event) => {
-        const selected = $(event.currentTarget).data()
-        const action = selected.action
-        const id = selected.charId
-        switch (action) {
-            case 'select-char':
-                const character = game.actors.get(id)
-                game.user.update({ "character": character })
-                this.close()
-                break;
-            case 'unselect-char':
-                game.user.update({ "character": null })
-                this.close()
-                break;
-            default:
-                ui.notifications.error('ACP | Encountered an invalid "data-action" in _handleButtonClick')
-        }
-    }
-
-    render(...args) {
-        super.render(...args)
-        setTimeout(() => {
-            document.getElementById('acp-char-search').focus()
-        }, 200)
-    }
-
-    async _updateObject(event, formData) {
-        this.options.query = formData.charSearch
-        this.render(true)
-    }
-}
-
 export default class CharacterSelectorV2 extends HandlebarsApplicationMixin(ApplicationV2) {
 
     static DEFAULT_OPTIONS = {
@@ -113,8 +14,8 @@ export default class CharacterSelectorV2 extends HandlebarsApplicationMixin(Appl
             handler: CharacterSelectorV2.#onSearch,
         },
         position: {
-            height: 800,
-            width: 500
+            height: 'auto',
+            width: 800
         },
         tag: 'form',
         window: {
@@ -146,15 +47,22 @@ export default class CharacterSelectorV2 extends HandlebarsApplicationMixin(Appl
         let characters = []
         let ownershipLevel = game.settings.get(ACP.ID, 'ownershipLevel')
         const actors = this.searchQuery ? game.actors.search({ query: this.searchQuery }) : game.actors
-        for (const actor of actors) {
-            if (actor.getUserLevel(this.user) >= ownershipLevel) characters.push(actor)
+        for (let actor of actors) {
+            let userLevel = actor.getUserLevel(this.user)
+            if (userLevel >= ownershipLevel) {
+                actor.ownedLevel = Object.keys(CONST.DOCUMENT_OWNERSHIP_LEVELS)[userLevel + 1]
+                characters.push(actor)
+            }
         }
+
+        const user = this.user
+        user.gm = this.user.isGM
 
         return {
             characters: characters,
             opts: opts,
             ...this.searchQuery && {searchQuery: this.searchQuery},
-            user: this.user
+            user: user
         }
     }
 
@@ -164,7 +72,6 @@ export default class CharacterSelectorV2 extends HandlebarsApplicationMixin(Appl
             const pref = this.user.getFlag(ACP.ID, ACP.FLAGS.SELECTOR)
             const base = ACP.getPosition(this.constructor)
             if (pref) {
-                options.position.height = pref.height
                 options.position.width = pref.width
             }
             options.position.left = pref ? pref.left : base.left
@@ -175,6 +82,8 @@ export default class CharacterSelectorV2 extends HandlebarsApplicationMixin(Appl
 
     // ADD RENDERED APP TO USER DOCUMENT
     async _onFirstRender(context, options) {
+        const searchBar = document.querySelector(`.acp-char-search_${this.user.id}`)
+        if (searchBar) searchBar.focus()
         return this.user.apps[this.id] = this
     }
 
@@ -211,7 +120,7 @@ export default class CharacterSelectorV2 extends HandlebarsApplicationMixin(Appl
         return this.close(true)
     }
 
-    // SET THIS.USERS SET CHARACTER TO NULL IF THEY HAVE ONE
+    // SET THIS.USERS SELECTED CHARACTER TO NULL IF THEY HAVE ONE
     static async #unSelect(event, target) {
         if (this.user.character) {
             await this.user.update({ character: null })
